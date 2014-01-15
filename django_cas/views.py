6f9945fda6f87128e7a7f172071f8da3b0ab8c03
Thomas Baguet <baguet@unistra.fr>
2014-01-15 17:07:18 +0100

    Fix issues with Django 1.5 django.http.get_host() is now request.get_host()
    see : https://docs.djangoproject.com/en/dev/ref/request-response/#django.http.HttpRequest.get_host

--- django_cas/views.py
+++ django_cas/views.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 """CAS login/logout replacement views"""
 from datetime import datetime
 from urllib import urlencode
 import urlparse
 from operator import itemgetter
 
-from django.http import get_host, HttpResponseRedirect, HttpResponseForbidden, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django_cas.models import PgtIOU
@@ -18,10 +20,10 @@ def _service_url(request, redirect_to=None, gateway=False):
     """Generates application service URL for CAS"""
 
     protocol = ('http://', 'https://')[request.is_secure()]
-    host = get_host(request)
+    host = request.get_host()
     prefix = (('http://', 'https://')[request.is_secure()] + host)
     service = protocol + host + request.path
-    if redirect_to:        
+    if redirect_to:
         if '?' in service:
             service += '&'
         else:
@@ -42,9 +44,9 @@ def _service_url(request, redirect_to=None, gateway=False):
                     if item[0] == item2[0]:
                         gateway_params.pop(index)
             extra_params = gateway_params + query_list
-            
+
             #Sort params by key name so they are always in the same order.
-            sorted_params = sorted(extra_params, key=itemgetter(0))            
+            sorted_params = sorted(extra_params, key=itemgetter(0))
             service += urlencode(sorted_params)
         else:
             service += urlencode({REDIRECT_FIELD_NAME: redirect_to})
@@ -55,15 +57,15 @@ def _redirect_url(request):
     """Redirects to referring page, or CAS_REDIRECT_URL if no referrer is
     set.
     """
-    
+
     next = request.GET.get(REDIRECT_FIELD_NAME)
     if not next:
         if settings.CAS_IGNORE_REFERER:
             next = settings.CAS_REDIRECT_URL
         else:
             next = request.META.get('HTTP_REFERER', settings.CAS_REDIRECT_URL)
-                        
-        host = get_host(request)
+
+        host = request.get_host()
         prefix = (('http://', 'https://')[request.is_secure()] + host)
         if next.startswith(prefix):
             next = next[len(prefix):]
@@ -92,7 +94,7 @@ def _logout_url(request, next_page=None):
     url = urlparse.urljoin(settings.CAS_SERVER_URL, 'logout')
     if next_page:
         protocol = ('http://', 'https://')[request.is_secure()]
-        host = get_host(request)
+        host = request.get_host()
         url += '?' + urlencode({'url': protocol + host + next_page})
     return url
 
@@ -116,14 +118,14 @@ def login(request, next_page=None, required=False, gateway=False):
         user = auth.authenticate(ticket=ticket, service=service)
 
         if user is not None:
-            #Has ticket, logs in fine    
+            #Has ticket, logs in fine
             auth.login(request, user)
             if settings.CAS_PROXY_CALLBACK:
                 proxy_callback(request)
             #keep urls
             return HttpResponseRedirect(next_page)
         elif settings.CAS_RETRY_LOGIN or required:
-            #Has ticket, 
+            #Has ticket,
             if gateway:
                 return HttpResponseRedirect(_login_url(service, ticket, True))
             else:
@@ -132,7 +134,7 @@ def login(request, next_page=None, required=False, gateway=False):
             #Has ticket, not session
             if getattr(settings, 'CAS_CUSTOM_FORBIDDEN'):
                 from django.core.urlresolvers import reverse
-                return HttpResponseRedirect(reverse(settings.CAS_CUSTOM_FORBIDDEN)+ "?" + request.META['QUERY_STRING'] )
+                return HttpResponseRedirect(reverse(settings.CAS_CUSTOM_FORBIDDEN) + "?" + request.META['QUERY_STRING'] )
             else:
                 error = "<h1>Forbidden</h1><p>Login failed.</p>"
                 return HttpResponseForbidden(error)
@@ -166,7 +168,7 @@ def logout(request, next_page=None):
     if cas_logout_request:
         session = _get_session(cas_logout_request[0])
         request.session = session
-        
+
     from django.contrib.auth import logout
     logout(request)
     if not next_page:
@@ -178,9 +180,9 @@ def logout(request, next_page=None):
 
 def proxy_callback(request):
     """Handles CAS 2.0+ XML-based proxy callback call.
-    Stores the proxy granting ticket in the database for 
+    Stores the proxy granting ticket in the database for
     future use.
-    
+
     NB: Use created and set it in python in case database
     has issues with setting up the default timestamp value
     """
@@ -190,7 +192,7 @@ def proxy_callback(request):
     if not (pgtIou and tgt):
         return HttpResponse('No pgtIOO', mimetype="text/plain")
     try:
-        PgtIOU.objects.create(tgt = tgt, pgtIou = pgtIou, created = datetime.now())
+        PgtIOU.objects.create(tgt=tgt, pgtIou=pgtIou, created=datetime.now())
         request.session['pgt-TICKET'] = ticket
         return HttpResponse('PGT ticket is: %s' % str(ticket, mimetype="text/plain"))
     except:
