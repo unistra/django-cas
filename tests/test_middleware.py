93a152fbe3413689b788f5da7d9a1fc20cbb5df3
v-eye <arnaud.grausem@unistra.fr>
2017-03-15 10:30:00 +0100

    prove the CAS_ADMIN_AUTH parameter usage
    refs #3

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,10 +1,26 @@
-from django.http import HttpRequest
-from django.test import TestCase, override_settings, modify_settings
-from django_cas.middleware import CASMiddleware
 from django.conf import settings
+from django.contrib.admin.sites import AdminSite
+from django.contrib.auth.models import AnonymousUser
+from django.test import TestCase, override_settings, modify_settings
+from django.http import HttpRequest, HttpResponseRedirect
+from django_cas.middleware import CASMiddleware
+from .views import test_anonymous_view, test_authenticated_view
 
 
 class TestCASMiddleware(TestCase):
+
+    def test_is_an_admin_view(self):
+        middleware = CASMiddleware()
+        self.assertTrue(
+            middleware._is_an_admin_view(AdminSite.index)
+        )
+
+    def test_is_a_normal_view(self):
+        middleware = CASMiddleware()
+        self.assertFalse(
+            middleware._is_an_admin_view(test_anonymous_view)
+        )
+
 
     @modify_settings(MIDDLEWARE_CLASSES={
         'remove': 'django.contrib.auth.middleware.AuthenticationMiddleware'
@@ -22,3 +38,30 @@ class TestCASMiddleware(TestCase):
             response = self.client.get('/anonymous/')
 
         self.assertIn('removed in version 1.1.5', str(cm.warning))
+
+    def test_normal_view(self):
+        middleware = CASMiddleware()
+        request = HttpRequest()
+        response = middleware.process_view(request, test_anonymous_view, (), {})
+        self.assertIsNone(response)
+
+    @override_settings(CAS_ADMIN_AUTH=False)
+    def test_normal_view_with_admin_cas_disabled(self):
+        middleware = CASMiddleware()
+        request = HttpRequest()
+        response = middleware.process_view(request, test_anonymous_view, (), {})
+        self.assertIsNone(response)
+
+    def test_admin_view(self):
+        middleware = CASMiddleware()
+        request = HttpRequest()
+        request.user = AnonymousUser()
+        response = middleware.process_view(request, AdminSite.index, (), {})
+        self.assertIsInstance(response, HttpResponseRedirect)
+
+    @override_settings(CAS_ADMIN_AUTH=False)
+    def test_admin_view_with_admin_cas_disabled(self):
+        middleware = CASMiddleware()
+        request = HttpRequest()
+        response = middleware.process_view(request, AdminSite.index, (), {})
+        self.assertIsNone(response)
